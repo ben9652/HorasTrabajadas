dc96512ef786fc905dd52dfbcaab42fd2494b461
Benjamín Boeri <ben9652@gmail.com>
2023-06-05 11:26:59 -0300

    Configuración manual de servidores hecha

--- scripts/Setup.bat
+++ scripts/Setup.bat
@@ -1,8 +1,8 @@
 @echo off
 
+call "%~dp0\includes\Servers\configServers.bat"
 call "%~dp0\includes\MySQL\MySQLsetup.bat"
 call "%~dp0\includes\Curl\CurlSetup.bat"
-call "%~dp0\includes\Servers\configServers.bat"
 
 pushd ..
 vendor\bin\premake5.exe vs2022
--- scripts/includes/Servers/configServers.bat
+++ scripts/includes/Servers/configServers.bat
@@ -2,17 +2,23 @@
 setlocal
 
 REM Leer la dirección IP de MySQLServer.txt
-set /p mysqlip=<%~dp0/MySQLServer.txt
+set /p mysqlip=<"%~dp0/MySQLServer.txt"
 
 REM Leer la dirección IP de ApacheServer.txt
-set /p apacheip=<%~dp0/ApacheServer.txt
+set /p apacheip=<"%~dp0/ApacheServer.txt"
+
+cd %~dp0\..\..\..
+mkdir servers
+cd servers
+for /f "delims=" %%i in ('cd') do set CURRENT_DIR=%%i
 
 REM Escribir las direcciones IP en servers.h
 (
+    echo #include ^<string^>
     echo #define MYSQL_IP "%mysqlip%"
     echo #define APACHE_IP "%apacheip%"
     echo const std::string mysql_ip = MYSQL_IP;
     echo const std::string apache_ip = APACHE_IP;
-) > ..\..\..\servers\servers.h
+) > "%CURRENT_DIR%\servers.h"
 
 endlocal
